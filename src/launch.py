fc82486b8336326632b3dcc830c18b8cf11a49e8
Karim Daoud <karim.daoud@epita.fr>
2018-12-05 00:24:16 +0100

    updated allocate function, still to be fully tested with complex cases

--- src/launch.py
+++ src/launch.py
@@ -7,8 +7,14 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 nb_children = 3
 node_size = 20
+PRINT_LOGS = True
+'''
+data field meaning:
+=> -10 : memory_initialisation
+=> 1 : Asking memory
 
 
+'''
 class Variable:
     def __init__(self, size):
         self.value = [None] * size
@@ -23,28 +29,47 @@ class Allocator:
         self.variables = set()
         self.allocated = 0
         self.clock = 0
+        self.memory_map = None
+        if self.children:
+            self.memory_map = dict([ (x, 0) for x in self.children])
 
     def allocate(self, size):
         if self.allocated + size < node_size:
             var = Variable(size)
             self.variables.add(var)
-            # notify the allocation to other processes
-            return var
-        else:
-            pass
+            self.allocated += size
 
-    def send(self, data, dest, tag):
+            # notify the allocation to other processes
+            if self.parent != None:
+                data = 'memory_allocation'
+                self.send(data, self.parent, 3, size=size)
+        else:
+            if self.parent != None:
+                data = 'request_allocation'
+                self.send(data, self.parent, 1, size=size)
+            else:
+                # deal with not enough memory left
+                pass
+
+    def send(self, data, dest, tag, val_id = None, size = None):
+        data = {'clock' : self.clock, 'data' : data, 'src' : rank}
+        if val_id:
+            data['id'] = val_id
+        if size:
+            data['size'] = size
         req = comm.isend(data, dest=dest, tag=tag)
         req.wait()
         self.clock += 1
-        print(rank, "send: ", data, dest)
+        if PRINT_LOGS:
+            print("send: ", data)
 
     def receive(self, src, tag):
         req = comm.irecv(source = src, tag = tag)
         data = req.wait()
-        self.clock = max(self.clock, data[0])
-        print(rank, 'received: ', data[1], src)
-        return data[1]
+        self.clock = max(self.clock, data['clock']) + 1
+        if PRINT_LOGS:
+            print('received: ', data)
+        return data
 
 
     def init_memory(self):
@@ -52,29 +77,38 @@ class Allocator:
         if self.parent != None:
             parent_data = self.receive(self.parent, 11)
             # memory request is represented by the int '-10' in the data field
-            if parent_data != -10:
+            if parent_data['data'] != 'init_memory':
                 print('incorrect data for memory initialisation')
 
         # if the node have children, send memory request and wait for answer
         if self.children:
             for child in self.children:
-                data = (self.clock, -10)
+                data = 'init_memory'
                 self.send(data, child, 11)
 
             for child in self.children:
                 data = self.receive(child, 11)
-                self.allocated += data
+                self.memory_map[child] = data['data']
+                self.allocated += data['data']
 
         # if node have again a parent, send all the info back to him
         if self.parent != None:
-            if parent_data == -10:
-                data = (self.clock, self.allocated)
+            if parent_data['data'] == 'init_memory':
+                data = self.allocated
                 self.send(data, self.parent, 11)
 
         # print to verify the allocated variable per node
-        if self.children:
-            print(rank, self.allocated)
+        print('end of init_memory for rank: ', rank, '; total allocated is: ', self.allocated, '; memory map is: ', self.memory_map)
 
+    def listen(self):
+        data = self.receive(MPI.ANY_SOURCE, MPI.ANY_TAG)
+        print('listened: ', data)
+
+        if data['data'] == 'request_allocation':
+            self.allocate(data['size'])
+        elif data['data'] == 'memory_allocation':
+            self.memory_map[data['src']] += data['size']
+            print('memory alloc', self.memory_map)
 
 class DummyApp:
     def __init__(self, allocator):
@@ -84,13 +118,17 @@ class DummyApp:
     def run(self):
         pass
 
-
 def main():
     allocator = Allocator()
     allocator.init_memory()
+    if rank == 1:
+        allocator.allocate(10)
+        allocator.allocate(19)
+    if rank == 0:
+        for i in range(2): #waiting for 2 messages, but should be a while true i think
+            allocator.listen()
 
     app = DummyApp(allocator)
-    app.run()
 
 
 if __name__ == "__main__":
